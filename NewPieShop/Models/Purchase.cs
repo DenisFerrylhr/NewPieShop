dc383f4dfbeac046d207572138bfd1f4a39908e9
Dieseled-UP <l00099023@student.lyit.ie>
2019-01-31 21:57:28 +0000

    Added new Purchase class

---
+++ NewPieShop/Models/Purchase.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NewPieShop.Models
+{
+    public class Purchase
+    {
+        public int Id { get; set; }
+        public int PieId { get; set; }
+        public int CustomerId { get; set; }
+        public Customer Customer { get; set; }
+    }
+}
