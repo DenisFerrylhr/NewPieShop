fe0c977badd5698c62b49f389f58f29942d1a62d
Dieseled-UP <l00099023@student.lyit.ie>
2019-01-31 21:55:59 +0000

    Added new Customer class

---
+++ NewPieShop/Models/Customer.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NewPieShop.Models
+{
+    public class Customer
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string AddressLine1 { get; set; }
+        public string Town { get; set; }
+        public string County { get; set; }
+        public string PostCode { get; set; }
+        public ICollection<Purchase> Purchases { get; set; }
+        public override string ToString()
+        {
+            return "Customer: " + Id + " " + "Name: " + Name;
+        }
+    }
+}
