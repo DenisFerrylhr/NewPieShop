579467f041c7cf48f344d5ed1a619a771bb5d8c1
Dieseled-UP <l00099023@student.lyit.ie>
2019-02-03 20:31:10 +0000

    Added new pie repository

---
+++ NewPieShop/Models/IPieRepository.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NewPieShop.Models
+{
+    interface IPieRepository
+    {
+        List<Pie> GetPies();
+        Pie GetPieById(int? id);
+        void AddNewPie();
+        void RemovePie(int? id);
+    }
+}
