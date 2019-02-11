3d6be56512b14ceac062df24e605a1c8ebe55a27
Dieseled-UP <l00099023@student.lyit.ie>
2019-02-11 15:47:14 +0000

    CSS and styling and layout updated

--- NewPieShop/wwwroot/js/site.js
+++ NewPieShop/wwwroot/js/site.js
@@ -2,3 +2,35 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+let angle = 0;
+
+img = document.querySelector('.logo-img');
+
+img.addEventListener('mouseenter', function () {
+    angle = (angle + 15) % 360;
+    img.classList.add('rotate' + angle);
+});
+
+img.addEventListener('mouseout', function () {
+    img.classList.remove('rotate15');
+    angle = 0;
+});
+
+window.addEventListener('resize', function () {
+    let width = this.window.innerWidth;
+    pies = document.querySelectorAll('.pie');
+
+    if (width <= 992) {
+        for (let i = 0; i < pies.length; i++) {
+            pies[i].classList.remove('col-md-4');
+            pies[i].classList.add('col-md-6');
+        }
+    }
+
+    if (width > 992) {
+        for (let i = 0; i < pies.length; i++) {
+            pies[i].classList.remove('col-md-6');
+            pies[i].classList.add('col-md-4');
+        }
+    }
+});
